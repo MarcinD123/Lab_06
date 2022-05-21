d93c6b179defc4402e70546019b2afaafc5307c8
Mdzi <marcindziedzic27@gmail.com>
2022-05-21 18:05:09 +0200

    Add project files.

---
+++ ListofUsers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Lab_06
+{
+     class ListofUsers
+    {
+        public string[] Names = new string[] { "Janusz", "Andrzej", "Władysław", "Domino", "Franciszek", "Mariusz", "Mateusz", "Damian", "Rafał", "Kamil", "Marek", "Michał", "Szymon", "Dariusz", "Tomasz" };
+        
+        public string[] Role = new string[] { "ADMIN", "MODERATOR", "TEACHER", "STUDENT" };
+
+        public int xd { get; set; }
+        public User Generator()
+        {
+            Random rnd = new Random();
+            var name = Names[rnd.Next(Names.Length)];
+
+            string rola; ;
+
+            int szansaNaRole = rnd.Next(0,100);
+            Console.WriteLine(szansaNaRole);
+            if (szansaNaRole>90)
+            {
+                rola = "ADMIN";
+            }
+            else if(szansaNaRole<=90&szansaNaRole>75)
+            { rola = "MODERATOR"; }
+            else if (szansaNaRole<=75&szansaNaRole>50)
+            {
+                rola = "TEACHER";
+            }
+            else
+            {
+                rola = "STUDENT";
+            }
+
+            var age = rnd.Next(20, 80);
+
+            int[] oceny = new int[rnd.Next(5, 10)];
+            for (int i = 0; i < oceny.Length; i++)
+            {
+                oceny[i] = rnd.Next(1, 7);
+                Thread.Sleep(5);
+            }
+
+            if (rola!="STUDENT")
+            {
+                oceny = null;
+            }
+            
+            Thread.Sleep(10);
+            DateTime created = new DateTime(1990, 1, 1).AddDays(rnd.Next(20, 500));
+            DateTime? deleted = new DateTime(2000, 1, 1).AddDays(rnd.Next(20, 500));
+            if (rnd.Next()%5==0)
+            {
+                deleted = null;
+            }
+            User user = new User(name, rola, age, oceny, created, deleted);
+            //xd++;
+            //Console.WriteLine(xd);
+            return user;
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            ListofUsers newusers = new ListofUsers();
+            int numberOfUsers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                
+                users.Add(newusers.Generator());
+            }
+            foreach (var item in users)  
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Role);                
+                if (item.Marks!=null)
+                {
+                    foreach (var mark in item.Marks)
+                    {
+                        Console.Write(mark+' ');
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("_______-");
+            }
+            //1.ilość rekordów w tablicy
+            Console.WriteLine("1:"+users.Count);
+
+
+
+            //2.Listę nazw użytkowników
+            var names = from user in users
+                        select user.Name;
+            Console.WriteLine("2:");
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //3.Posortowanych użytkowników po nazwach
+            var groupByName = users.OrderBy(x => x.Name);
+            Console.WriteLine("3:");
+            foreach (var item in groupByName)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+
+            //4.Listę dostępnych ról użytkowników
+            Console.WriteLine("4");
+            var listOfRoles = from person in users
+                              group person by person.Role into newGroup
+                              orderby newGroup.Key
+                              select newGroup;
+            
+            foreach (var item in listOfRoles)
+            {
+                Console.WriteLine(item.new);
+            }
+
+        }
+    }
+}
